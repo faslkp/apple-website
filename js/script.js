29549bb6bf33b8b202c0627699cfcbaea5aaf65d
Fasalu <faslkp@gmail.com>
2024-12-23 11:35:43 +0530

    hover-menu added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
     interval: 2000,
     touch: false
 })
+function showHoverMenu(){
+    var hoverMenu = document.getElementById("hoverMenu");
+    hoverMenu.style.zIndex = "100";
+    hoverMenu.style.display = "block";
+}
+function hideHoverMenu(){
+    var hoverMenu = document.getElementById("hoverMenu");
+    hoverMenu.style.zIndex = "-100";
+    hoverMenu.style.display = "none";
+}
